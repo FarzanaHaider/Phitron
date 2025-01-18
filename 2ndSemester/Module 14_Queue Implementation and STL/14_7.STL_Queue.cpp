1413a2d7cdd1d5819f43c8954e11bbd1e73c829e
FarzanaHaider <farzanahaider1425@gmail.com>
2025-01-18 12:26:41 +0600

    Completed Module 14

---
+++ 2ndSemester/Module 14_Queue Implementation and STL/14_7.STL_Queue.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+# define opt() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+# define ll long long
+# define endl '\n'
+
+int main()
+{
+    queue<int>q;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        int val;
+        cin>>val;
+        q.push(val);
+    }
+    while(!q.empty())
+    {
+        cout<<q.front()<<endl;
+        q.pop();
+    }
+    return 0;
+}
